03df4c046eea3b07fde377131c9f2ec0e8401401
Herdy Dwi Putra <herdydwiputra@gmail.com>
2019-07-22 11:47:27 +0700

    Updates

---
+++ precache-manifest.2dafbea888c99c326a03a97d6e38d2c0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f100d0bc4a7e9b984b324a471c87415",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "a64b9a8e5919a3e486fc",
+    "url": "/monsters-rolodex/static/css/main.072faf50.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "a64b9a8e5919a3e486fc",
+    "url": "/monsters-rolodex/static/js/main.0f2a683b.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
